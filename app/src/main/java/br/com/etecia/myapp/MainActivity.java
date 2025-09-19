c86cbf2488487c25955e6fabdaae9bf7823cc071
lndsilva <lndsilva@hotmail.com>
2025-09-18 22:14:44 -0300

    drawer

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -1,24 +1,96 @@
 package br.com.etecia.myapp;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawer_layout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // Initialize the DrawerLayout, Toolbar, and NavigationView
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.nav_view);
+
+        // Create an ActionBarDrawerToggle to handle
+        // the drawer's open/close state
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.nav_open, R.string.nav_close);
+
+        // Add the toggle as a listener to the DrawerLayout
+        drawerLayout.addDrawerListener(toggle);
+
+        // Synchronize the toggle's state with the linked DrawerLayout
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.nav_account) {
+                    // Show a Toast message for the Account item
+                    Toast.makeText(MainActivity.this,
+                            "Account Details", Toast.LENGTH_SHORT).show();
+                }
+
+                if (menuItem.getItemId() == R.id.nav_settings) {
+                    // Show a Toast message for the Settings item
+                    Toast.makeText(MainActivity.this,
+                            "Settings Opened", Toast.LENGTH_SHORT).show();
+                }
+
+                if (menuItem.getItemId() == R.id.nav_logout) {
+                    // Show a Toast message for the Logout item
+                    Toast.makeText(MainActivity.this,
+                            "You are Logged Out", Toast.LENGTH_SHORT).show();
+                }
+
+                // Close the drawer after selection
+                drawerLayout.closeDrawers();
+                // Indicate that the item selection has been handled
+                return true;
+            }
+        });
+
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            // Called when the back button is pressed.
+            @Override
+            public void handleOnBackPressed() {
+                // Check if the drawer is open
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    // Close the drawer if it's open
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    // Finish the activity if the drawer is closed
+                    finish();
+                }
+            }
+        });
+
     }
 }
