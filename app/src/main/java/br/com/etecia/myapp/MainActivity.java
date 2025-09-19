20b0d16a655aa84e0bfedcebb4667be0bfe11346
lndsilva <lndsilva@hotmail.com>
2025-09-18 22:17:49 -0300

    drawer

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -33,60 +33,59 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        // Initialize the DrawerLayout, Toolbar, and NavigationView
+
         drawerLayout = findViewById(R.id.drawer_layout);
         toolbar = findViewById(R.id.toolbar);
         navigationView = findViewById(R.id.nav_view);
 
-        // Create an ActionBarDrawerToggle to handle
-        // the drawer's open/close state
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawerLayout, toolbar, R.string.nav_open, R.string.nav_close);
 
-        // Add the toggle as a listener to the DrawerLayout
+
         drawerLayout.addDrawerListener(toggle);
 
-        // Synchronize the toggle's state with the linked DrawerLayout
+
         toggle.syncState();
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 if (menuItem.getItemId() == R.id.nav_account) {
-                    // Show a Toast message for the Account item
+
                     Toast.makeText(MainActivity.this,
                             "Account Details", Toast.LENGTH_SHORT).show();
                 }
 
                 if (menuItem.getItemId() == R.id.nav_settings) {
-                    // Show a Toast message for the Settings item
+
                     Toast.makeText(MainActivity.this,
                             "Settings Opened", Toast.LENGTH_SHORT).show();
                 }
 
                 if (menuItem.getItemId() == R.id.nav_logout) {
-                    // Show a Toast message for the Logout item
+
                     Toast.makeText(MainActivity.this,
                             "You are Logged Out", Toast.LENGTH_SHORT).show();
                 }
 
-                // Close the drawer after selection
+
                 drawerLayout.closeDrawers();
-                // Indicate that the item selection has been handled
+
                 return true;
             }
         });
 
         getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
-            // Called when the back button is pressed.
+
             @Override
             public void handleOnBackPressed() {
-                // Check if the drawer is open
+
                 if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-                    // Close the drawer if it's open
+
                     drawerLayout.closeDrawer(GravityCompat.START);
                 } else {
-                    // Finish the activity if the drawer is closed
+
                     finish();
                 }
             }
